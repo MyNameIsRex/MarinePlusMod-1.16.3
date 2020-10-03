b9f8fa5d14032c34571bc0e59468a6cb8d84d2b6
MyNameIsRex <72241200+MyNameIsRex@users.noreply.github.com>
2020-10-03 09:42:06 +0800

    Update #2: Raw Tuna Fish Item added to code

--- src/main/java/com/mnir/marineplus/items/RawTunaItem.java
+++ src/main/java/com/mnir/marineplus/items/RawTunaItem.java
@@ -2,14 +2,17 @@ package com.mnir.marineplus.items;
 
 import net.minecraft.item.Food;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 
-public class RawTunaItem extends AbstractModFishItem
+public class RawTunaItem extends Item
 {
     public RawTunaItem()
     {
-        super(new Item.Properties().food((new Food.Builder()
+        super(new Item.Properties().group(ItemGroup.FOOD).food((new Food.Builder()
                 .hunger(2)
                 .saturation(0.2F)
+                .setAlwaysEdible()
+                .meat()
                 .build())));
     }
 }
